6a26438e2da22eed1687aea9a515b24bfb008a6a
cveizhans <45264624+cveizhans@users.noreply.github.com>
2019-02-18 09:22:17 +0100

    'xmlread'

--- ViewModels/OrangeViewModel.cs
+++ ViewModels/OrangeViewModel.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MultipleViews.ViewModels
 {
     public class OrangeViewModel
     {
+        /// <summary>
+        /// read xml
+        /// </summary>
+        public void ReadXml(string file)
+        {
+            List<String> names = new List<String>();
+
+            var xml = File.ReadAllText(file);
+            var parsed = XElement.Parse(xml);
+            var employee = parsed.Elements(XName.Get("Mitarbeiter"));
+            var ordered = employee.OrderBy(i => i.Element("Nachname")?.Value ?? string.Empty);
+
+            foreach (var o in ordered)
+            {
+                names.Add(o.Element("Nachname")?.Value + ", " + o.Element("Vorname"));
+            }
+        }
+
+        /// <summary>
+        /// read json
+        /// </summary>
+        public void ReadJson()
+        {
+
+        }
     }
 }
