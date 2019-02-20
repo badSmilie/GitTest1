692a7ed9bdc876665c26943b9e00d369ba4d0b29
cveizhans <45264624+cveizhans@users.noreply.github.com>
2019-02-20 18:46:24 +0100

    'json'

--- ViewModels/OrangeViewModel.cs
+++ ViewModels/OrangeViewModel.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using MultipleViews.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -10,6 +13,13 @@ namespace MultipleViews.ViewModels
 {
     public class OrangeViewModel
     {
+        public PlantCollection PlantCol { get; set; }
+
+        public OrangeViewModel()
+        {
+            PlantCol = ReadJsonFile();
+        }
+
         /// <summary>
         /// read xml
         /// </summary>
@@ -31,9 +41,31 @@ namespace MultipleViews.ViewModels
         /// <summary>
         /// read json
         /// </summary>
-        public void ReadJson()
+        public void ReadJsonWeb()
         {
+            // web
+            using (var webClient = new WebClient())
+            {
+                // get a String representation of our json
+                String rawJSON = webClient.DownloadString("http://plantplaces.com/perl/mobile/viewplantsjson.pl?Combined_Name=Redbud");
+                // convert the JSON string to a series of objects
+                PlantCollection plantCollection = JsonConvert.DeserializeObject<PlantCollection>(rawJSON);
+                // Do some computation
+                Console.WriteLine(plantCollection.Plants.Count);
+            }   
+        }
 
+        /// <summary>
+        /// read json
+        /// </summary>
+        public PlantCollection ReadJsonFile()
+        {
+            using (StreamReader r = new StreamReader(@"Data\plants.json"))
+            {
+                string rawJSON = r.ReadToEnd();
+                PlantCollection plantCollection = JsonConvert.DeserializeObject<PlantCollection>(rawJSON);
+                return plantCollection;
+            }
         }
     }
 }
